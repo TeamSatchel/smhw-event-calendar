6763dd5bdd843cd6dfee5b45fd59bbecaa93efc1
Eugene Grigorenko <Eugene.Grigorenko@dataart.com>
2018-03-02 12:05:25 +0200

    Set up webpack and React
    - Remove javascript used by pipeline
    - Require pack in layout
    - Add javasript directory structure
    - Add configuration for webpack and react

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,33 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import CalendarDashboard from "../components/CalendarDashboard";
+
+const Hello = props => (
+    <div>Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+    name: 'David'
+}
+
+Hello.propTypes = {
+    name: PropTypes.string
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <CalendarDashboard />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
+
