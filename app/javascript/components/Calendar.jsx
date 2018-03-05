e442e3f9dd2dd9d394747b84f6f2a04753f6400a
Eugene Grigorenko <Eugene.Grigorenko@dataart.com>
2018-03-05 15:16:29 +0200

    Group events by rows in calendar
    - Find all events that cross current week
    - Sort them by StartDate
      - Could be sorted by created_at, so events that were created first would be displeyed on top
    - Convert events in the next format
     - [[e1, e2,... eN], [e11, e12, ... eX] ...]
        - Where e1 is an event that is displayed on the first row
          - e2 is displayed on the first row, after e1 has ended
        - e11 is displayed on the beginning of the second row
        - e12 is displayed on the second row after e11
        - etc.
    - Iterate over current week
      - Create CalendarDay for each day of the week
      - Pass all of the events related to each day by row
      - If some rows have no events pass undefined(result of Array#find call)

--- app/javascript/components/Calendar.jsx
+++ app/javascript/components/Calendar.jsx
@@ -8,6 +8,7 @@ export default class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.startOfWeek = moment().startOf('isoWeek');
+    this.endOfWeek = moment().endOf('isoWeek');
   }
 
   render() {
@@ -19,21 +20,57 @@ export default class Calendar extends React.Component {
   }
 
   computeWeekDays() {
-    let weekDays = [];
+    const weekDays = [];
+    const eventsByRows = this.groupEventsByRows();
     for (let dayNumber=0; dayNumber<7; dayNumber++) {
       const currentDate = moment(this.startOfWeek).add(dayNumber, 'd');
-      const events = this.props.events.filter(event => (
-        currentDate.isBetween(event.attributes.startDate, event.attributes.endDate, 'day', '[]')
-      ));
+      const eventsOnDayByRows = eventsByRows.map(eventsRow => {
+        return eventsRow.find(event => {
+          const startDate = moment(event.attributes.startDate);
+          const endDate = moment(event.attributes.endDate);
+          return currentDate.isBetween(startDate, endDate, 'd', '[]');
+        });
+      });
+
       weekDays.push(
-        <CalendarDay key={currentDate.unix()} date={currentDate} events={events}/>
+        <CalendarDay
+          key={currentDate.unix()}
+          date={currentDate}
+          events={eventsOnDayByRows} />
       );
     }
     return weekDays;
   }
+
+  groupEventsByRows() {
+    const eventsOnWeek = this.eventsHappenOnWeek().sort((a, b) => (
+      moment(a.attributes.startDate) - moment(b.attributes.startDate)
+    ));
+    return eventsOnWeek.reduce((acc, item) => {
+      let rowFound = false;
+      acc.map((events) => {
+        let lastEventInRow = events[events.length - 1];
+        if (moment(lastEventInRow.attributes.endDate).isBefore(moment(item.attributes.startDate))) {
+          rowFound = true;
+          events.push(item);
+        }
+        return events;
+      });
+      if (!rowFound) acc.push([item]);
+      return acc;
+    }, []);
+  }
+
+  eventsHappenOnWeek() {
+    return this.props.events.filter(event => {
+      let startDate = moment(event.attributes.startDate);
+      let endDate = moment(event.attributes.endDate);
+      return startDate.isBetween(this.startOfWeek, this.endOfWeek, 'd', '[]') ||
+             endDate.isBetween(this.startOfWeek, this.endOfWeek, 'd', '[]') ||
+             (startDate.isBefore(this.startOfWeek, 'd') && endDate.isAfter(this.endOfWeek, 'd'));
+    });
+  }
 }
-
-
 
 Calendar.defaultProps = {
   events: [],
