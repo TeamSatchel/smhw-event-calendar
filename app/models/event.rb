025cc2e42ec6c6f4329498d711ddab9855654e93
Artem Orel <arfelio1980@gmail.com>
2018-03-18 13:39:05 +0200

    add scope for actual events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ActiveRecord::Base
+
+  scope :actual, ->(beginning_of_week, end_of_week) {
+    where("start_date BETWEEN :beginning_of_week
+      AND :end_of_week
+      OR start_date < :beginning_of_week
+      AND end_date BETWEEN :beginning_of_week
+      AND :end_of_week
+      OR end_date > :beginning_of_week",
+      beginning_of_week: beginning_of_week, end_of_week: end_of_week)
+  }
 end
