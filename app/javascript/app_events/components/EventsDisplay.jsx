774af950ede4e36a09c4bc38858c08c550de69f9
Phil Brockwell <nineteen40eight@gmail.com>
2018-04-01 18:32:36 +0100

    Incorporate components into EventsDisplay

--- app/javascript/app_events/components/EventsDisplay.jsx
+++ app/javascript/app_events/components/EventsDisplay.jsx
@@ -1,100 +1,78 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import queryString from 'query-string';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import Calendar from "./Calendar";
+import Controls from "./Controls";
+import MessageBar from "./MessageBar";
+
+const TODAY = new Date();
 
 class EventsDisplay extends React.Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       events: [],
-      display_dates: [],
-      previous_date: '',
-      next_date: ''
+      displayDates: [],
+      previousDate: null,
+      nextDate: null
     };
   }
 
-  fetchEvents (startDate) {
-    axios.get( `api/events?start_date=${startDate}` )
+  fetchEvents(newStartDate) {
+    this.startDate = newStartDate;
+
+    axios
+      .get(`api/events?start_date=${this.startDate}`)
       .then(response => {
-        this.setState(
-                       { 
-                         events: response.data['events'],
-                         display_dates: response.data['display_dates'],
-                         previous_date: response.data['previous_date'],
-                         next_date: response.data['next_date']
-                       }
-                      );
+        this.setState({
+          events: response.data["events"],
+          displayDates: response.data["display_dates"],
+          previousDate: response.data["previous_date"],
+          nextDate: response.data["next_date"]
+        });
+
+        console.log(response);
       })
       .catch(error => {
+        this.messageBar.update(error.message, true);
+
         console.error(error);
       });
   }
 
-  setStartDateFromQueryString (qs) {
-    this.qsParams = queryString.parse(qs);
-    if (this.qsParams.start_date) {
-      this.startDate = this.qsParams.start_date;
-    } else {
-      this.startDate = new Date();
-    }
+  createEvent(startDate, description, lengthDays) {
+    axios
+      .post(
+        `api/events?event[start_date]=${startDate}&event[description]=${description}&event[length_days]=${lengthDays}`
+      )
+      .then(response => {
+        this.fetchEvents(startDate);
+        this.messageBar.update(response.data.message);
+      })
+      .catch(error => {
+        this.messageBar.update(error.message, true);
+      });
   }
 
-  componentDidMount () {
-    this.startDate = new Date();
-    this.fetchEvents(this.startDate);
+  componentDidMount() {
+    this.fetchEvents(TODAY);
   }
 
-  componentWillReceiveProps (nextProps) {
-    this.setStartDateFromQueryString(nextProps.location.search);
-    this.fetchEvents(this.startDate);
-  }
-
-  render () {
-    var display_dates = this.state.display_dates;
+  render() {
     return (
       <div>
-        <Link className='date-changer left-date-changer' to={`/?start_date=${this.state.previous_date}`}>&lt;</Link>
-
-        <table className='calendar'>
-          <tbody>
-            <tr>
-              {display_dates.map(function(date, i) {
-                if (date.weekend) {
-                  return <th key={i} className='weekend'>
-                    <p>{date.name}</p>
-                    <p>{date.day_with_month}</p>                     
-                    </th>;
-                } else {
-                  return <th key={i}>
-                    <p>{date.name}</p>
-                    <p>{date.day_with_month}</p>                     
-                    </th>;
-                }
-              })}
-            </tr>
-
-            {this.state.events.map(function(event, i){
-              var count = 0;
-              return <tr key={i}>
-                {display_dates.map(function(date, i) {
-                  if (event.start_date === date.date || event.end_date === date.date || (event.start_date < date.date && event.end_date > date.date)) {
-                    count++
-                    if (count === 1) {
-                      return <td key={i} className='scheduled-event'>{event.description}</td>;
-                    } else {
-                      return <td key={i} className='scheduled-event'></td>;
-                    }
-                  } else {
-                    return <td key={i}></td>;
-                  }
-                })}
-              </tr>;
-            })}
-          </tbody>
-        </table>
-
-        <Link className='date-changer right-date-changer' to={`/?start_date=${this.state.next_date}`}>&gt;</Link>
+        <Calendar
+          displayDates={this.state.displayDates}
+          events={this.state.events}
+          today={TODAY}
+        />
+        <Controls
+          previousDate={this.state.previousDate}
+          nextDate={this.state.nextDate}
+          updateDates={this.fetchEvents.bind(this)}
+          createEvent={this.createEvent.bind(this)}
+          startDate={TODAY}
+        />
+        <MessageBar ref={ref => (this.messageBar = ref)} />
       </div>
     );
   }
