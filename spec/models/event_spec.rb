4d4c3d4e19e69304b47e7a52a15fe620b082a136
Sergey Kastornov <s.kastornov@sumatosoft.com>
2018-10-18 19:00:10 +0300

    Add Event dates checking for a correct range.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  describe 'Validations' do
+  describe 'validations' do
     context 'if :title is not defined' do
       let(:event) { build(:event, title: nil) }
 
@@ -26,11 +26,31 @@ RSpec.describe Event, type: :model do
       end
     end
 
-    context 'if all necessary fields are defined' do
+    context 'if :ends_at is less than :starts_at' do
+      let(:event) { build(:event, starts_at: Date.today, ends_at: Date.today - 1.day) }
+      let(:error_message) { 'The start date should be less or equal than the end date' }
+
+      it 'event is invalid' do
+        expect(event.valid?).to eq(false)
+        expect(event.errors[:base]).to include(error_message)
+      end
+    end
+
+    context 'if all necessary fields are defined and correct' do
       let(:event) { build(:event) }
 
-      it 'event should be invalid' do
-        expect(event.valid?).to eq(true)
+      context 'when :starts_at less than :ends_at' do
+        it 'event is valid' do
+          expect(event.valid?).to eq(true)
+        end
+      end
+
+      context 'when :starts_at equal to :ends_at' do
+        let(:event) { build(:event, starts_at: Date.today, ends_at: Date.today) }
+
+        it 'event is valid' do
+          expect(event.valid?).to eq(true)
+        end
       end
     end
   end
