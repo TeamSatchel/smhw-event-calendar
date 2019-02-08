d7209492a47a845a0ab4c95a254c1c75ff92ce55
Andrii Baran <andriy.baran.v@gmail.com>
2019-02-08 11:59:13 +0200

    Added logic for rendering existing event and displaying just created

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -57,4 +57,41 @@ $(document).on('ready', function(){
       $('#summary').append('<strong>Description: </strong>'+ event.target.value +'<br>');
     }
   })
+
+
+  $.each($('tbody').data().events, function( index, value ) {
+    addEvent(value);
+  })
+  adjustTable();
+
+  $('form').on('ajax:success', function(e, data){
+    addEvent(data);
+    eventHeight = $('.event#'+data.id).outerHeight();
+    $('tbody td').height(($('tbody td').height() + eventHeight));
+    $('#summary').empty();
+    $('#create').addClass('hidden');
+    $('.event').css({top: -$('tbody td').height()});
+    $('#event-wizard-start').removeClass('hidden');
+    e.target.reset();
+  })
 })
+
+function addEvent(event) {
+  thisWeekBegin = $('tbody td#'+event.begins);
+  thisWeekEnd = $('tbody td#'+event.ends);
+  days = (Date.parse(event.ends) - Date.parse(event.begins)) / 86400000
+  if (thisWeekBegin.length > 0) {
+    if (thisWeekEnd.length > 0) {
+      visibilityWidth = $('table').width() / 7 * (days + 1) - 35
+      up = thisWeekBegin.position().top;
+      left = thisWeekBegin.position().left;
+      $('#overlay').append('<div id="'+event.id+'"class="event" style="left:'+left+'px; width:'+visibilityWidth+'px;"><strong>'+event.title+'</strong><br>'+event.description+'</div>')
+    }
+  }
+}
+
+function adjustTable() {
+  $('tbody td').css({ height: $('#overlay').height() });
+  $('.event').css({ top: -($('#overlay').height() / 2) });
+  $('#overlay').height($('#overlay').height() / 2);
+}
