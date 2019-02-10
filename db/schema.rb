06955a2ed19c4d262ea34a70bcc5d688b9dbeb1b
Sergiy Parkhuts <serparkhuts@gmail.com>
2019-02-10 18:50:28 +0200

    install pg gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190129203840) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
