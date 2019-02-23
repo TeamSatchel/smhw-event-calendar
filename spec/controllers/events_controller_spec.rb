667ef500c61a6e8b5667680c078ba98ade8d0357
David Rosa Moncayo <davidrosa@mangrove.nl>
2019-02-23 03:40:18 +0000

    feat(events): Add tests for create action

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -10,4 +10,77 @@ RSpec.describe EventsController do
       it { expect(response).to have_http_status(:success) }
     end
   end
+
+  describe 'GET #create' do
+    context 'if all data is present' do
+      before :each do
+        post :create, params: {
+          event: {
+            title: 'Event title',
+            description: 'description',
+            start_date: '01-02-2019',
+            end_date: '02-02-2019',
+          },
+        }
+      end
+
+      it 'returns created and creates the event' do
+        expect(response).to have_http_status(:created)
+        expect(Event.count).to eq(1)
+      end
+    end
+
+    context 'if some data is not present' do
+      before :each do
+        post :create, params: {
+          event: {
+            title: 'Event title',
+            start_date: '01-02-2019',
+            end_date: '02-02-2019',
+          },
+        }
+      end
+
+      it 'returns created and does not create the event' do
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(Event.count).to eq(0)
+      end
+    end
+
+    context 'if the dates are not correct' do
+      before :each do
+        post :create, params: {
+          event: {
+            title: 'Event title',
+            description: 'description',
+            start_date: 'no date',
+            end_date: '02-02-2019',
+          },
+        }
+      end
+
+      it 'returns created and does not create the event' do
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(Event.count).to eq(0)
+      end
+    end
+
+    context 'if start date is later than end date' do
+      before :each do
+        post :create, params: {
+          event: {
+            title: 'Event title',
+            description: 'description',
+            start_date: '05-02-2019',
+            end_date: '02-02-2019',
+          },
+        }
+      end
+
+      it 'returns created and does not create the event' do
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(Event.count).to eq(0)
+      end
+    end
+  end
 end
