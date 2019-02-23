10c014d89073c2c848dab5cab582ca7e7ee965d2
David Rosa Moncayo <davidrosa@mangrove.nl>
2019-02-23 00:36:07 +0000

    feat(calendar): Add all info to events

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,13 +5,20 @@ $(document).ready(function($) {
   $('#calendar').fullCalendar({
     defaultView: 'basicWeek',
     firstDay: 1,
+    allDayDefault: true,
     eventSources: [
       {
         url: '/events.json',
         color: 'yellow',
         textColor: 'black'
       }
-    ]
+    ],
+    eventRender: function(event, element) {
+      if (!!event.subject)
+        element.find('.fc-content').append('<br><span><strong>' + event.subject + '</strong></span>');
+      if (!!event.description)
+        element.find('.fc-content').append('<br><span>' + event.description + '</span>');
+    }
   });
 
 });
