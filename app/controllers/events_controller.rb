e78745976a5c0c25edea9062a793ff8209f992c5
Stuart Tee <sktee93@gmail.com>
2019-02-25 02:00:24 +0800

    Create Event model and migrations

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,19 @@
 class EventsController < ApplicationController
+  # GET /events
+  # GET /events.json
   def index
+    @events = Event.all
   end
+  # POST /events
+  # POST /events.json
+  def create
+
+  end
+
+
+
+  private
+     def event_params
+      params.require(:event).permit(:title, :description, :start_date, :end_date)
+    end
 end
