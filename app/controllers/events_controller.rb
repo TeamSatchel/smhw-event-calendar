9ef3b3e52bdb5ddbd4f19b980f7a7447ece05813
Ivan Sheremeta <iisheremeta@gmail.com>
2019-02-24 04:34:30 +0200

    refactor events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,18 +2,27 @@ class EventsController < ApplicationController
   helper_method :events
 
   def index
-    json_response(events)
+    json_response(events: events)
   end
 
   def create
-    event = Event.create(event_params)
-    json_response(event, :created)
+    event = Event.create!(event_params)
+    json_response({ event: event }, :created)
   end
 
   private
 
   def events
-    Event.all
+    events = Event.all
+    if params[:from_date]
+      events =
+        events.where('DATE(start_time) >= ?', Date.parse(params[:from_date]))
+    end
+    if params[:to_date]
+      events =
+        events.where('DATE(end_time) <= ?', Date.parse(params[:to_date]))
+    end
+    events
   end
 
   def event_params
