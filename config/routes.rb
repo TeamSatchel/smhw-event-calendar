2bd5cdbe5c558ac1fca4bc9e976e83e0627e40cf
yaroslavh <yaroslav.polatayko@gmail.com>
2019-03-09 23:15:24 +0200

    Add Ember integration directly to Rails project * init Ember app in root folder as 'frontend' * update Rails routes for ember app * add Event model and basic components

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount_ember_app :frontend, to: "/"
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :events, only: [:index, :create]
