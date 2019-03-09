e6a2f503dd4e5c3aab52d23ecdbbf01d1e6b20d6
yaroslavh <yaroslav.polatayko@gmail.com>
2019-03-09 23:28:37 +0200

    add API call to load data

--- frontend/app/routes/events.js
+++ frontend/app/routes/events.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-
+    return Ember.$.getJSON('api/v1/events').then(function(response) {
+      return Ember.RSVP.hash({ events: response['data'] });
+    });
   }
 });
