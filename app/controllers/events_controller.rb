cbc93633c0e668bec866ceb48f10a834bba2bd81
Jose Antonio Parra <josparbar@gmail.com>
2019-03-10 18:05:31 +0100

    EventsController actions, index and create. Also, serializer for Event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,15 @@
 class EventsController < ApplicationController
   def index
+    @events = ActiveModelSerializers::SerializableResource.new(Event.all)
+  end
+
+  def create
+    @event = Event.create(event_params)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_date, :end_date)
   end
 end
