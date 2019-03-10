320ad69f4b903cf741df8acdb73d6e1a0a74db9f
Jose Antonio Parra <josparbar@gmail.com>
2019-03-10 21:38:22 +0100

    Include functional tests.

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -5,4 +5,12 @@ class EventsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test 'should create an event' do
+    assert_difference('Event.count') do
+      post :create, format: :js, event: { title: 'New Event', description: 'Description for event', start_date: Time.now, end_date: Time.now + 2.days }
+    end
+
+    assert_response :success
+  end
 end
