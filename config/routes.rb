273e330656d1896388242bb122c67fd526ae926e
Alexander Tkachuk <mail@tksasha.me>
2019-04-19 18:01:21 +0300

    tiny cleanuped config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events, only: %i(index create)
 
-  resources :events, only: %i(index)
-
-  root to: redirect('/events')
+  root to: 'events#index'
 end
