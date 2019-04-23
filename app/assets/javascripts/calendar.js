b4a5541c23d0d5c2bea39f0e8960300baabfade1
G C <gladkontakthof@gmail.com>
2019-04-23 19:04:35 +0000

    Create partials and callback for new event form

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -8,7 +8,16 @@ var create_calendar = function() {
       firstDay: 1, //Starts on Monday
       columnHeaderHtml: function(mom) {
           return '<p>' + mom.format('dddd') + '</p> <p>' + mom.format('Do MMM') + '</p>';
+      },
+      selectable: true, //Makes calendar selectable, so we can use "select:" for adding functionality
+      select: function(start, end) {
+        //Calls script for creating new event
+        $.getScript('/events/new', function() {
+          console.log('callbak works!');
+        });
       }
     });
 };
+
+//Listen for page to load and then creates calendar
 $(document).on('turbolinks:load', create_calendar);
