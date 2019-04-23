9cb40aa4c81ea790300cdb59c7c3caf255375f09
G C <gladkontakthof@gmail.com>
2019-04-23 20:34:05 +0000

    Create Event model using rails generate

---
+++ spec/factories/events.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :event do
+    title { "MyString" }
+    description { "MyText" }
+    start_date { "2019-04-23 19:14:13" }
+    end_date { "2019-04-23 19:14:13" }
+  end
+end
