a52725f8e0051a36679597a503e2415c3df4ef8b
Andrey Zastela <azastela@Andreys-MacBook-Pro.local>
2019-05-06 23:47:50 +0300

    Add events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,26 @@
 class EventsController < ApplicationController
-
+  # GET /events
   def index
+    @events = Event.all
   end
 
+  # POST /events.json
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.fetch(:event, {}).permit(:title, :description, :start_date,
+      :end_date, :date_range, :start_time, :end_time)
+  end
 end
