f485e492c508933579e37a38bbc073fef2d50e01
Tony Rom <thetonyrom@gmail.com>
2019-05-13 21:20:45 +0300

    Clean up tests

--- frontend/app/helpers/validation-message.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { capitalize } from '@ember/string';
-
-export function validationMessage([error]) {
-  return capitalize(`${error.attribute} ${error.message}`);
-}
-
-export default helper(validationMessage);
--- frontend/tests/integration/components/add-event-button-test.js
+++ frontend/tests/integration/components/add-event-button-test.js
@@ -7,20 +7,8 @@ module('Integration | Component | add-event-button', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{add-event-button}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#add-event-button}}
-        template block text
-      {{/add-event-button}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.textContent.trim(), '+');
   });
 });
