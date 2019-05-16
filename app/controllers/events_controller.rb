aefb9acfc6c68e4ab6a104e7fa00cc5e650192c8
Petro <petro.yakubiv@gmail.com>
2019-05-16 21:53:13 +0300

    Add event templates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,25 @@
 class EventsController < ApplicationController
   respond_to :html, :json
+
   def index
-    @events = Event.where(start_at: params[:start]..params[:end])
+    @events = Event.all
     respond_with(@events)
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+  end
+
+  private
+
+  def event_params
+    params.require(:event)
+          .permit(:start_at, :end_at, :description, :date_range)
   end
 
 end
