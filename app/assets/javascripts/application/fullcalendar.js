bf1227ed73f509cd3f55ebbf887d18640bd32cc4
Petro <petro.yakubiv@gmail.com>
2019-05-16 19:43:48 +0300

    Add event model

--- app/assets/javascripts/application/fullcalendar.js
+++ app/assets/javascripts/application/fullcalendar.js
@@ -1,7 +1,12 @@
 $(document).on('turbolinks:load', function () {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: ['dayGrid']
+    plugins: ['dayGrid'],
+    header: {
+        left: '',
+        center: 'title'
+      },
+    events: '/events.json',
   });
 
   calendar.render();
