71e86582f803a104e9f77da92e2b3aec2cb72967
Bohdan Pohorilets <bohdan.pohorilets@ilabsolutions.com>
2019-05-16 20:25:59 +0300

    [FEAT] Added interactions to calendar
    1. Added ability to add new events
    2. Made events source for a calendar
    3. Added basic validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  validates :start_date, presence: true
+
+  def as_json(*)
+    { id: id, start: start_date, end: end_date, title: description }
+  end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -5,7 +5,19 @@ require 'rails_helper'
 describe EventsController do
   context '#create' do
     it 'is available for everyone' do
-      post :create, xhr: true
+      post :create, xhr: true, params: { event: { start: Time.current } }
+      expect( response ).to have_http_status( :success )
+    end
+  end
+
+  context '#index' do
+    it 'renders a calendar' do
+      get :index
+      expect( response ).to have_http_status( :success )
+    end
+
+    it 'serves a list of all events' do
+      get :index, format: :json
       expect( response ).to have_http_status( :success )
     end
   end
