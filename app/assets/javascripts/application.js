961e68e20b53abab450ca685a168b1dbf7bdbc69
Petro <petro.yakubiv@gmail.com>
2019-05-16 19:14:08 +0300

    Add fullcalendar and moment js libs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 //= require jquery3
 //= require jquery_ujs
 // require rails-ujs
+//= require moment
+//= require fullcalendar/main
 //= require activestorage
 //= require turbolinks
 //= require_tree ./application
