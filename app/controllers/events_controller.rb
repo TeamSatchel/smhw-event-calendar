0de08b2acee03fc2345d50bbc9f7202ac3568576
Nerian <siotopo@gmail.com>
2019-05-20 14:08:26 +0200

    Prototype UI. New event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
 
   def index
+    @event = Event.new(start_date: Time.current, end_date: Time.current.advance(hours: 3))
+  end
+
+  def new
+
   end
 
 end
