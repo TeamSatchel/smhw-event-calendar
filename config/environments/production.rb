79df39fed258062126df3df5e3a7ef875e6d67e1
Kirill Kalachev <dhampik@gmail.com>
2019-05-23 14:11:18 +0400

    Switch to barebone API application
    - remove assets
    - use API version of controllers
    - remove helpers (not needed at the moment)
    - add application_record to inherit models from
    - remove views and layout as it's an API
    - remove handling of assets from environment and initializers
    - remove handling of csp & cookies from initializers (not need for API)
    - remove public directory (no need for API)
    - replace rspec and spec directory with default minitest\
    (not sure if I'll actually use it, can be switched back later if needed)
    - update ruby version to 2.6.3
    - update rails and gems for API version of application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,15 +22,6 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
@@ -61,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "qwe_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "event-calendar_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
@@ -83,7 +74,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
