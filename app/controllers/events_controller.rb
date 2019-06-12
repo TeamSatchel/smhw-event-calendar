fcb1b0efa46e80cf6aa14473390704ad0a8222bd
nitinvupk <nitinvupk83@gmail.com>
2019-06-12 20:58:33 +0530

    Create and render event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,17 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
+    @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+    @event.save
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_date, :end_date)
+  end
 end
---
+++ app/views/events/_event.json.jbuilder
@@ -0,0 +1,3 @@
+json.extract! event, :id, :title, :description
+json.start event.start_date
+json.end event.end_date
