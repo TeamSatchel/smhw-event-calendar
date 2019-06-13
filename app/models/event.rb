bdf9d0c5555fe5b1db11cc0ab93fffa69021904d
nitinvupk <nitinvupk83@gmail.com>
2019-06-13 16:04:03 +0530

    presence true validation on fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,14 @@
 class Event < ActiveRecord::Base
-  validate :end_date_cannot_be_less_than_to_start_date
+  validates :title, :description, :start_date, :end_date, presence: true
+  validate :end_date_cannot_be_less_than_to_start_date, if: :date_present?
 
   def end_date_cannot_be_less_than_to_start_date
     if end_date.to_date < start_date.to_date
       errors.add(:end_date, " should be greater than start date.")
     end
+  end
+
+  def date_present?
+    start_date.present? && end_date.present?
   end
 end
