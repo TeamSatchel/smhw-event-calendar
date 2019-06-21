195396039c8513b7e0b7fa629fa7910a74bb600f
Vadym Motsukh <vadym.motsukh@gmail.com>
2019-06-21 19:56:19 +0300

    prepare calendars

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['blue', 'green', 'red', 'purple'].each do |color|
+  Calendar.create(name: color.capitalize, color: color)
+end
